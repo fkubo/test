6441664bc2e433fc816daf5c6d0b5c950affd174
Fumihiro Kubo <kubo.fumihiro@ns-sol.co.jp>
2012-04-26 15:20:50 +0900

    追加

---
+++ SpringMVCTemplate/src/test/java/org/spring/sample/HomeControllerTest.java
@@ -0,0 +1,14 @@
+package org.spring.sample;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HomeControllerTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
